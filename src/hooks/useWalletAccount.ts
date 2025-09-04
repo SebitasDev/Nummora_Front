9bc7326ac90a4c427b261acf410ad7f3c4a0ab30
SebasDev <sebastian.ramirez@blackbirdlabs.com.co>
2025-09-04 15:51:38 -0500

    feat: add user state management in useWalletAccount hook

--- src/hooks/useWalletAccount.ts
+++ src/hooks/useWalletAccount.ts
@@ -1,19 +1,29 @@
+import { useEffect, useState } from "react";
 import { http, useAccount, useWalletClient } from "wagmi";
-import {createPublicClient, createWalletClient} from "viem";
-import {liskSepolia} from "@reown/appkit/networks";
+import { createPublicClient, createWalletClient } from "viem";
+import { liskSepolia } from "@reown/appkit/networks";
 
 export const useWalletAccount = () => {
     const { isConnected, address } = useAccount();
     const { data: walletClient } = useWalletClient();
+    const [user, setUser] = useState<`0x${string}` | null>(null);
+
+    useEffect(() => {
+        if (isConnected && address) {
+            setUser(address as `0x${string}`);
+        } else {
+            setUser(null);
+        }
+    }, [isConnected, address]);
 
     const client = createWalletClient({
         chain: liskSepolia,
-        transport: http()
+        transport: http(),
     });
 
     const publicClient = createPublicClient({
         chain: liskSepolia,
-        transport: http()
+        transport: http(),
     });
 
     return {
@@ -21,6 +31,6 @@ export const useWalletAccount = () => {
         client,
         walletClient,
         publicClient,
-        user: address,
+        user,
     };
 };
