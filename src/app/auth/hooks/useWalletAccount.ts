9e636781d2a7aa28f2591ef198f5d874ed9e70a0
SebasDev <sebastian.ramirez@blackbirdlabs.com.co>
2025-10-01 10:14:42 -0500

    feat: enhance signMessage to handle both hex and plain string messages

--- src/app/auth/hooks/useWalletAccount.ts
+++ src/app/auth/hooks/useWalletAccount.ts
@@ -4,14 +4,25 @@ import {Address, hexToBytes, toBytes} from "viem";
 export const useWalletAuth = () => {
   const { isConnected, user, walletClient } = useWalletAccount();
 
-  const signMessage = async (message: `0x${string}`) => {
+  const signMessage = async (message: string | `0x${string}`) => {
     if (!walletClient || !user) {
       throw new Error("Wallet not connected");
     }
-    const signature = await walletClient.signMessage({
-      account: walletClient.account,
-      message: { raw: hexToBytes(message) },
-    });
+
+    let signature: `0x${string}`;
+
+    if (message.startsWith("0x")) {
+      signature = await walletClient.signMessage({
+        account: walletClient.account,
+        message: { raw: hexToBytes(message as `0x${string}`) },
+      });
+    } else {
+      signature = await walletClient.signMessage({
+        account: walletClient.account,
+        message,
+      });
+    }
+
     return { signature, user: user as Address };
   };
 
